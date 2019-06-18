a70f5c8779c34b821ee3cbf52cb8fcd1ccec82d1
adabutch <adabutch1@gmail.com>
2019-06-18 09:41:36 -0400

    patch security, temp rm dist

--- static/sw.js
+++ static/sw.js
@@ -6,16 +6,16 @@ workbox.precaching.precacheAndRoute([
     "revision": "9549321e190f30a898963ed709657ab9"
   },
   {
-    "url": "/_nuxt/9950538d0caa19d0b2af.js",
-    "revision": "a4a06cf2dffc9b8d0e3a292c8b8cf9e2"
+    "url": "/_nuxt/2354e3b430aae9d9f169.js",
+    "revision": "18245c143553c1242e0ac67a6f219aaf"
+  },
+  {
+    "url": "/_nuxt/a86d1ecda0fa0929e53f.js",
+    "revision": "be4338b94d72dfffc2439ce3f312798c"
   },
   {
     "url": "/_nuxt/b756030d5a4dd4230179.js",
     "revision": "78bdcb09697221e2384e75e3211528cf"
-  },
-  {
-    "url": "/_nuxt/d6fc6c159d1ff536fc8d.js",
-    "revision": "8cb8b982a0aab42ff2073aa932c9b3d5"
   }
 ], {
   "cacheId": "bead",
