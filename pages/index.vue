8d316e6d10727cbb0412484e4902710d3fc2b5df
adabutch <adabutch1@gmail.com>
2019-11-08 08:22:29 -0500

    fix: google map embed

--- pages/index.vue
+++ pages/index.vue
@@ -312,7 +312,7 @@ export default {
 
       // Row Eight
       rowEight: {
-        iframeSrc:  'https://www.google.com/maps/d/embed?mid=1aDrFXV_RFPD0GsOjxDyCFqcvQyA'
+        iframeSrc:  'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6186.391278288097!2d-86.53615174249369!3d39.1702729409047!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886c66debfabde5f%3A0x4594beb75fd3ef88!2s401%20N%20Morton%20St%2C%20Bloomington%2C%20IN%2047404!5e0!3m2!1sen!2sus!4v1573219230342!5m2!1sen!2sus'
       },
 
       // Row Nine
